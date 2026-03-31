1080dd88222251eb3e10b2dcc80ba0011f148125
alexguerrag <63695270+alexguerrag@users.noreply.github.com>
2026-03-31 10:53:36 -0300

    test: add settings module smoke test

---
+++ test/features/settings/di/settings_module_test.dart
@@ -0,0 +1,12 @@
+import 'package:finaper/features/settings/di/settings_module.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('SettingsModule initializes without throwing', () async {
+    final module = SettingsModule();
+
+    await module.register();
+
+    expect(module.settingsController, isNotNull);
+  });
+}
